b97deee4980c1db62220f6fdca2e2a4ac5df9c5c
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-24 07:54:14 -0500

    added mongo

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  return <footer>Dashboard kPage</footer>;
 };
 
 export default DashboardPage;
--- app/navbar/page.jsx
+++ app/navbar/page.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import Links from "../../components/Links.jsx";
+import Link from "next/link";
+import Links from "@/components/Links";
+import { Menu } from "lucide-react";
+import { FaGoogle } from "react-icons/fa";
 
 const NavBar = () => {
   return (
     <nav className="flex justify-between h-32 text-2xl p-4 text-white bg-gray-800">
       {/* MOBILE MENU */}
+      <div>
+        <Menu />
+      </div>
 
-      <div>Logo</div>
+      <div>
+        <Link href="/">Logo</Link>
+      </div>
       <div>
         <Links />
       </div>
-      <div>Login</div>
+      <div>
+        <FaGoogle />
+        Login or Register
+      </div>
     </nav>
   );
 };
