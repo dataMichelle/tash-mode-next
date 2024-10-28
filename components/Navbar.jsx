93db264afa8dca2436c179800ec87062d946122e
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-28 14:02:02 -0500

    fixed navbar link spacing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,17 +14,15 @@ const Navbar = () => {
     <nav className="border-b">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 xl:px-2">
         <div className="relative flex h-20 items-center justify-between">
+          {/* Mobile menu button */}
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-            {/* Mobile menu button */}
             <button
               type="button"
-              id="mobile-dropdown-button"
               className="relative inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
-              aria-expanded="false"
+              aria-expanded={isMobileMenuOpen ? "true" : "false"}
               onClick={() => setIsMobileMenuOpen((prev) => !prev)}
             >
-              <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open Main Menu</span>
               <svg
                 className="block h-6 w-6"
@@ -42,95 +40,80 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            {/* Logo */}
+
+          {/* Logo */}
+          <div className="flex items-center">
             <Link className="flex flex-shrink-0 items-center" href="/">
               <Image className="h-10 w-auto" src={logo} alt="Tash Mode Logo" />
               <span className="hidden md:block text-black text-2xl font-bold ml-2">
                 Tash Mode
               </span>
             </Link>
-            {/* Desktop Menu hidden below md screens */}
-            <div className="hidden md:ml-6 md:block">
-              <div className="flex space-x-2">
-                <Link
-                  href="/products"
-                  className="hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  Shop Tash Mode
-                </Link>
-                <Link
-                  href="/products"
-                  className="hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  Shop by Category
-                </Link>
-                <Link
-                  href="/products"
-                  className="hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  Shop by Culture
-                </Link>
-              </div>
-            </div>
           </div>
-          {/* Right side menu logged out */}
-          <div className="hidden md:block md:ml-6">
-            <div className="flex items-center">
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-3">
-                <FaGoogle className="mr-2" />
-                <span>Login or Register</span>
-              </button>
-            </div>
+
+          {/* Centered Menu Links */}
+          <div className="hidden md:flex md:flex-1 justify-center space-x-4">
+            <Link
+              href="/products"
+              className="hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+            >
+              Shop Tash Mode
+            </Link>
+            <Link
+              href="/products"
+              className="hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+            >
+              Shop by Category
+            </Link>
+            <Link
+              href="/products"
+              className="hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+            >
+              Shop by Culture
+            </Link>
           </div>
-          {/* Right side menu Logged In */}
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+
+          {/* Right side menu */}
+          <div className="flex items-center space-x-4">
+            <button className="hidden md:flex items-center text-white bg-gray-700 hover:bg-gray-900 rounded-md px-3 py-2">
+              <FaGoogle className="mr-2" />
+              <span>Login or Register</span>
+            </button>
+
             {/* Profile dropdown button */}
-            <div className="relative ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                  onClick={() => setIsProfileMenuOpen((prev) => !prev)}
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <Image
-                    className="h-8 w-8 rounded-full"
-                    src={profileImg}
-                    alt="User Profile"
-                    width={40}
-                    height={40}
-                  />
-                </button>
-              </div>
-              {/*  Profile Dropdown  */}
+            <div className="relative">
+              <button
+                type="button"
+                className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                aria-expanded={isProfileMenuOpen ? "true" : "false"}
+                aria-haspopup="true"
+                onClick={() => setIsProfileMenuOpen((prev) => !prev)}
+              >
+                <span className="sr-only">Open user menu</span>
+                <Image
+                  className="h-8 w-8 rounded-full"
+                  src={profileImg}
+                  alt="User Profile"
+                  width={40}
+                  height={40}
+                />
+              </button>
               {isProfileMenuOpen && (
                 <div
-                  id="user-menu"
-                  className="absolute right-0 w-48 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className="absolute right-0 w-48 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
                   role="menu"
                   aria-orientation="vertical"
-                  aria-labelledby="user-menu-button"
-                  tabIndex="-1"
                 >
                   <Link
                     href="/profile"
                     className="block px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-0"
                   >
                     Your Profile
                   </Link>
                   <button
                     className="block px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-1"
                   >
                     Sign Out
                   </button>
@@ -140,21 +123,30 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
       {/* Mobile menu, show/hide based on menu state */}
       {isMobileMenuOpen && (
-        <div id="mobile-menu">
+        <div id="mobile-menu" className="md:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
             <Link
-              href="/"
-              className="block hover:text-white rounded-md px-3 my-4 hover:bg-gray-700"
+              href="/products"
+              className="block hover:text-white rounded-md px-3 py-2 hover:bg-gray-700"
             >
-              Home
+              Shop Tash Mode
             </Link>
-            <Link href="/products" className="block">
-              Shop
+            <Link
+              href="/products"
+              className="block hover:text-white rounded-md px-3 py-2 hover:bg-gray-700"
+            >
+              Shop by Category
             </Link>
-
-            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-3">
+            <Link
+              href="/products"
+              className="block hover:text-white rounded-md px-3 py-2 hover:bg-gray-700"
+            >
+              Shop by Culture
+            </Link>
+            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 rounded-md px-3 py-2">
               <FaGoogle className="mr-2" />
               <span>Login or Register</span>
             </button>
